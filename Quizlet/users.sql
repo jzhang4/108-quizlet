1fcc04983e35d760ad15314caad6c2a7dd9f401f
Serena Wong <serenaw@stanford.edu>
2016-02-29 17:42:04 -0800

    updated User/Account Manager

---
+++ Quizlet/users.sql
@@ -0,0 +1,11 @@
+USE c_cs108_jzhang4;
+
+DROP TABLE IF EXISTS users;
+ -- remove table if it already exists and start from scratch
+
+CREATE TABLE users (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	username CHAR(64) NOT NULL,
+	passwordHash CHAR(64) NOT NULL,
+	PRIMARY KEY (id)
+);
